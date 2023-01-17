5a290292b6bd04bd991baa431f78beedc562cf26
asunder <Loydzx11p@gmail.com>
2023-01-17 00:18:49 -0400

    Small additions to splashes.txt and bugfix to the Magma Totem

--- src/main/java/com/infinium/server/items/custom/misc/InfiniumTotemItem.java
+++ src/main/java/com/infinium/server/items/custom/misc/InfiniumTotemItem.java
@@ -5,6 +5,7 @@ import com.infinium.server.items.custom.InfiniumItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -26,12 +27,17 @@ public class InfiniumTotemItem extends Item implements InfiniumItem {
         super.inventoryTick(stack, world, entity, slot, selected);
         if (!(entity instanceof ServerPlayerEntity p)) return;
         var entityAttributeInstance = p.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+
         if (hasMagmaTotem(p)) {
-            if (!entityAttributeInstance.hasModifier(EXTRA_HEALTH_BOOST)) entityAttributeInstance.addTemporaryModifier(EXTRA_HEALTH_BOOST);
+            if (!entityAttributeInstance.hasModifier(EXTRA_HEALTH_BOOST)) {
+                entityAttributeInstance.addTemporaryModifier(EXTRA_HEALTH_BOOST);
+                p.setHealth(p.getHealth());
+
+            }
         } else {
             if (entityAttributeInstance.hasModifier(EXTRA_HEALTH_BOOST)) {
                 entityAttributeInstance.removeModifier(EXTRA_HEALTH_BOOST);
-                p.setHealth(p.getMaxHealth());
+                p.damage(DamageSource.OUT_OF_WORLD, 0.0001f);
             }
         }
     }
