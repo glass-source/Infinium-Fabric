27416efe8793bfd0d3ffa13a686c4b29bd407f57
asunder <Loydzx11p@gmail.com>
2023-05-26 00:23:27 -0400

    Removed include dependencies from build.gradle to optimize .jar file size and tweaked some values in GrapplingHookItem

--- src/main/java/com/infinium/server/items/custom/tools/utility/GrapplingHookItem.java
+++ src/main/java/com/infinium/server/items/custom/tools/utility/GrapplingHookItem.java
@@ -25,17 +25,16 @@ public class GrapplingHookItem extends Item implements InfiniumItem, Vanishable 
         var vec = user.getRotationVector().add(user.getVelocity());
 
         if (user.fishHook != null) {
-
             if (!world.isClient) {
                 user.fishHook.use(itemStack);
             }
-            var y = vec.getY() < 1 ? 0.25f : vec.getY();
-            user.setVelocity(vec.getX() * 1.7, y * 2.5, vec.getZ() * 1.7);
+
+            user.setVelocity(vec.getX() * 1.75f, vec.getY() * 2.5f, vec.getZ() * 1.75f);
             user.incrementStat(Stats.USED.getOrCreateStat(this));
             world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.05f);
             world.emitGameEvent(user, GameEvent.FISHING_ROD_REEL_IN, user);
             itemStack.damage(1, user, (p) -> p.sendToolBreakStatus(hand));
-            cooldownManager.set(this, 50);
+            cooldownManager.set(this, 40);
 
         } else {
             world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 1.0F, 0.05f);
@@ -43,6 +42,7 @@ public class GrapplingHookItem extends Item implements InfiniumItem, Vanishable 
                 var hook = new FishingBobberEntity(user, world, 0, 0);
                 world.spawnEntity(hook);
                 hook.setNoGravity(true);
+                hook.setGlowing(true);
             }
             world.emitGameEvent(user, GameEvent.FISHING_ROD_CAST, user);
         }
