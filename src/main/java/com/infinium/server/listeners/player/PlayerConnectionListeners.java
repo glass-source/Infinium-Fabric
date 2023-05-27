b3efa8adea9a9a844a1caa15a67d23c83ffc1d41
asunder <Loydzx11p@gmail.com>
2023-05-27 17:33:39 -0400

    Re-added panorama

--- src/main/java/com/infinium/server/listeners/player/PlayerConnectionListeners.java
+++ src/main/java/com/infinium/server/listeners/player/PlayerConnectionListeners.java
@@ -156,7 +156,8 @@ public class PlayerConnectionListeners {
         Azurex("257cc308-e6ca-485a-8445-065de91bee1c"),
         hheavensnight("e16dfef6-10db-44cd-adf4-5e2f8096c9f1"),
         Darkvid("03c803e0-20ae-411f-85d7-43ec52766d0e"),
-        DarthG_("072ee52f-661b-4940-8f25-c42ca521e4bc")
+        DarthG_("072ee52f-661b-4940-8f25-c42ca521e4bc"),
+        zzBeser("acc5b2f0-a7ad-472e-a557-e852c3651637")
         ;
         private final UUID uuid;
         WhitelistedPlayers(String playerUuid) {
